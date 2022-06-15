c5abc666ca68aa8019acb992bcfdbe1f8a75922a
thecodervaibhav <vaibhavghawale007@gmail.com>
2022-06-15 13:27:31 +0530

    payment page update with js some work is pending

--- payment-vaibhav/common js/payment.js
+++ payment-vaibhav/common js/payment.js
@@ -1,14 +1,22 @@
+
+
+//  1...........
+
 let homeDelivery =document.getElementById("home-delivery").addEventListener("click",HomeDelivery)
 
 function HomeDelivery(){
     console.log("hello")
 }
 
+// 2...............
+
 let storeDelivery= document.getElementById("store-delivery").addEventListener("click",StoreDelivery)
 
 function StoreDelivery(){
     console.log("hi")
 }
+
+//  3....Debit And Credit Card Div...............
 
 let debitCard=document.getElementById("debit-card").addEventListener("click",DebitCard)
 function DebitCard(){
@@ -21,3 +29,67 @@ function DebitCard(){
         card_details.style.display="none"
     }
 }
+
+//  4.......EMI div.....................
+
+let EMI=document.getElementById("emi").addEventListener("click",Emi)
+function Emi(){
+    let card_details=document.querySelector("#emi-card-1")
+    if(card_details.style.display==="none")
+    {
+        card_details.style.display="block";
+    }
+    else{
+        card_details.style.display="none"
+    }
+}
+
+
+//  4.......Net Banking-div.....................
+
+let netBank=document.getElementById("net-bank").addEventListener("click",NetBank)
+function NetBank(){
+    let card_details=document.querySelector("#net-banking-div")
+    if(card_details.style.display==="none")
+    {
+        card_details.style.display="block";
+    }
+    else{
+        card_details.style.display="none"
+    }
+}
+
+//  4.......Wallet-div.....................
+
+let Wallet =document.getElementById("wallets").addEventListener("click",WalletInput)
+
+function WalletInput(){
+    let card_details=document.querySelector("#wallet-div")
+    if(card_details.style.display==="none")
+    {
+        card_details.style.display="block";
+    }
+    else{
+        card_details.style.display="none"
+    }
+}
+
+    let Wallet_input =document.getElementById("wallet-div").addEventListener("click",WalletOpt)
+    function WalletOpt(){
+    let card_details=document.querySelector("#wallet-div-info")
+    if(card_details.style.display==="none")
+    {
+        card_details.style.display="block";
+    }
+    else{
+        card_details.style.display="none"
+    }
+
+}
+
+
+// 5. UPI..................................................../......................
+
+
+
+
