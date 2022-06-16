57e847e06c30a611d7110218aae236f4f2f47a9d
thecodervaibhav <vaibhavghawale007@gmail.com>
2022-06-16 21:39:52 +0530

    new update

--- payment-vaibhav/common js/payment.js
+++ payment-vaibhav/common js/payment.js
@@ -3,10 +3,14 @@
 //  1...........................................................................................//
 
 let homeDelivery =document.getElementById("home-delivery").addEventListener("click",HomeDelivery)
-let HideDiv=document.getElementById("delivery-info").style.display="block"
+ homeDelivery =document.getElementById("home-delivery").style.border="2px solid orange"
+
+// const api_key="07a51eea324d30f39c87a220dc518de8";
 
 function HomeDelivery(){
-    let HideDiv=document.getElementById("delivery-info").style.display="block"
+    
+    window.location.reload()
+    
 }
 
 // 2.................................................................................................//
@@ -14,9 +18,19 @@ function HomeDelivery(){
 let storeDelivery= document.getElementById("store-delivery").addEventListener("click",StoreDelivery)
 
 function StoreDelivery(){
-   let HideDiv=document.getElementById("delivery-info").style.display="none"
+   let HideDiv1=document.getElementById("delivery-info").style.display="none";
+   let HideDiv2=document.getElementById("location-info").style.display="block";
+   
+   let homeDelivery =document.getElementById("home-delivery").style.border="1px solid #bdc6ce";
+   let storeDelivery= document.getElementById("store-delivery").style.border="2px solid orange"
+    let city=document.getElementById("city_search").value;
 
-}
+
+
+            let iframe=document.getElementById("gmap_canvas");
+            iframe.src=`https://maps.google.com/maps?q=${city}&t=&z=13&ie=UTF8&iwloc=&output=embed`
+
+        }
 
 //  3....Debit And Credit Card Div.....................................................................//
 
@@ -31,6 +45,16 @@ function DebitCard(){
         card_details.style.display="none"
     }
 }
+
+// let debit_card=document.getElementById("card-details").addEventListener("click",stop)
+// function stop(){
+//     let stopdiv=document.getElementById("card-details")
+//     if(stopdiv.style.display==="none"){
+//         stopdiv.style.display==="block"
+//     }else{
+//         stopdiv.style.display==="none"
+//     }
+// }
 
 //  4.......EMI div.....................
 
@@ -105,3 +129,25 @@ function upi_Pay(){
 }
 
 
+//.......................for input tag ..................
+
+let input_tag=document.querySelector("input").addEventListener("input",checkInput)
+
+function checkInput(){
+    let input_tag=document.querySelector("input")
+    let input_value=document.querySelector("input").value;
+    if(input_value===null){
+       console.log("hello")
+    }else{
+        input_tag.style.borderBlock="rad"
+    }
+    
+}
+
+//..............................button Event...........................................................
+
+let button_show=document.getElementById("pay-btn").addEventListener("click",ShowStatus)
+
+function ShowStatus(){
+    
+}
