74f604a6c4d18edbe9c712407e85f3713e82eb4b
thecodervaibhav <vaibhavghawale007@gmail.com>
2022-06-17 18:42:35 +0530

    newly update files

--- payment-vaibhav/common js/payment.js
+++ payment-vaibhav/common js/payment.js
@@ -1,6 +1,6 @@
 
 
-//  1...........................................................................................//
+//  1.....................................Deliveery options......................................................//
 
 let homeDelivery =document.getElementById("home-delivery").addEventListener("click",HomeDelivery)
  homeDelivery =document.getElementById("home-delivery").style.border="2px solid orange"
@@ -13,7 +13,7 @@ function HomeDelivery(){
     
 }
 
-// 2.......................Search City By Input..........................................................................//
+// 2.............................................Search City By Input..........................................................................//
 
 
 
@@ -39,13 +39,9 @@ function StoreDelivery(){
    }
 }
 
+//  3...................................................Debit And Credit Card Div.....................................................................//
 
-
-
-
-//  3....Debit And Credit Card Div.....................................................................//
-
-let debitCard=document.getElementById("debit-card").addEventListener("click",DebitCard)
+let debitCard=document.getElementById("debit-card").addEventListener("dblclick",DebitCard)
 function DebitCard(){
     let card_details=document.querySelector("#card-details")
     if(card_details.style.display==="none")
@@ -57,19 +53,9 @@ function DebitCard(){
     }
 }
 
-// let debit_card=document.getElementById("card-details").addEventListener("click",stop)
-// function stop(){
-//     let stopdiv=document.getElementById("card-details")
-//     if(stopdiv.style.display==="none"){
-//         stopdiv.style.display==="block"
-//     }else{
-//         stopdiv.style.display==="none"
-//     }
-// }
+//  4........................................................EMI div.........................................................
 
-//  4.......EMI div.....................
-
-let EMI=document.getElementById("emi").addEventListener("click",Emi)
+let EMI=document.getElementById("emi").addEventListener("dblclick",Emi)
 function Emi(){
     let card_details=document.querySelector("#emi-card-1")
     if(card_details.style.display==="none")
@@ -82,9 +68,9 @@ function Emi(){
 }
 
 
-//  4.......Net Banking-div......................................................................//
+//  5.............................................................Net Banking-div......................................................................//
 
-let netBank=document.getElementById("net-bank").addEventListener("click",NetBank)
+let netBank=document.getElementById("net-bank").addEventListener("dblclick",NetBank)
 function NetBank(){
     let card_details=document.querySelector("#net-banking-div")
     if(card_details.style.display==="none")
@@ -96,9 +82,9 @@ function NetBank(){
     }
 }
 
-//  4.......Wallet-div..............................................................................//
+//  6...........................................................Wallet-div..............................................................................//
 
-let Wallet =document.getElementById("wallets").addEventListener("click",WalletInput)
+let Wallet =document.getElementById("wallets").addEventListener("dblclick",WalletInput)
 
 function WalletInput(){
     let card_details=document.querySelector("#wallet-div")
@@ -111,7 +97,7 @@ function WalletInput(){
     }
 }
 
-    let Wallet_input =document.getElementById("wallet-div").addEventListener("click",WalletOpt)
+    let Wallet_input =document.getElementById("select-wallet").addEventListener("click",WalletOpt)
     function WalletOpt(){
     let card_details=document.querySelector("#wallet-div-info")
     if(card_details.style.display==="none")
@@ -125,9 +111,9 @@ function WalletInput(){
 }
 
 
-// 5. UPI..................................................../......................
+// 7. ............................................................UPI..................................................../......................
 
-let UPI_pay=document.getElementById("upi").addEventListener("click",upi_Pay)
+let UPI_pay=document.getElementById("upi").addEventListener("dblclick",upi_Pay)
 function upi_Pay(){
     let card_details=document.querySelector("#Upi-Opt")
     if(card_details.style.display==="none")
@@ -140,10 +126,8 @@ function upi_Pay(){
 }
 
 
-//.......................for input tag ..................
 
-
-//..............................button Event...........................................................
+//.7..............................for input tag.........................................................................//
 
 let button_show=document.getElementById("pay-now").addEventListener("click",ShowStatus)
 
@@ -162,23 +146,64 @@ let UserArray=[]
         
     }
     UserArray.push(UserData)
-    console.log(UserArray)
+
+    console.log(UserArray[0].Uname)
+
+    let check_inform=document.getElementById("final-confirmation")
+
+    let h2=document.createElement("h2");
+    h2.innerText="Please confirmed Below details";
+
+    let name=document.createElement("p")
+    name.innerText=`Name:${UserArray[0].Uname}`
+
+    let contact=document.createElement("p")
+    contact.innerText=`Contact No:${UserArray[0].contactNo}`;
+   
+
+    let pincode=document.createElement("p")
+    pincode.innerText=`Pincode No.:${UserArray[0].pincode}`
+
+    let city=document.createElement("p")
+    city.innerText=`City:${UserArray[0].City}`
+
+    let state=document.createElement("p")
+    state.innerText=`State:${UserArray[0].state}`
+
+    let buildingname=document.createElement("p")
+    buildingname.innerText=`Building Name:${UserArray[0].buildingName}`
+
+    let flat=document.createElement("p")
+    flat.innerText=`Flat No.:${UserArray[0].flat}`
     
+    let com_btn=document.createElement("button")
+    com_btn.id="checck_info"
+    com_btn.innerText="Conform"
+    com_btn.addEventListener("click",Csheck_Info)
 
-
-
-
-
-
-
-
+    console.log(h2,name,flat,buildingname,city,state,pincode,contact)
 
     let div_show =document.getElementById("out-put")
     div_show.style.display="block"
+
+    check_inform.append(h2,name,flat,buildingname,city,state,pincode,contact,com_btn)
+    div_show.append(check_inform)
+    
+//8............................................button Event.............................................................//
+
+    function Csheck_Info(){
+    let check_inform=document.getElementById("final-confirmation")
+    check_inform.style.display="none"
+
     let time =document.getElementById("order-process");
     time.style.display="block"
+
+    let note_1=document.getElementById("note-1")
+    note_1.innerText=`Thanks you ${UserArray[0].Uname} for Shoping with us. Your Order confirmation is pending`
+
+ 
     let count=5;
-let id=setInterval(function(){
+    let id=setInterval(function(){
     count--;
     if(count===0){
     clearInterval(id);
@@ -186,12 +211,19 @@ let id=setInterval(function(){
     time.style.display="none"
     let div_show =document.getElementById("order-placed")
     div_show.style.display="block"
+    let note_2=document.getElementById("note-2")
+    note_2.innerText=`Congratualtions ${UserArray[0].Uname} Your order has been placed succesfully`
 }
 },1000);
 
 let more_shop=document.getElementById("more-shop").addEventListener("click",moreShop)
 function moreShop(){
    window.location.reload()
+} 
 }
 
 }
+
+//................................. Data Append to ..........................................................................//
+
+
